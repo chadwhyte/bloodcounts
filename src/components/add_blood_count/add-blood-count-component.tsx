f6c8296834585f1068999c4e349754eff7599e0d
Chad Whyte <chad.whyte@gmail.com>
2018-10-21 09:18:47 -0600

    Stubbing out more componenents and package infrastructure.

--- src/components/add_blood_count/add-blood-count-component.tsx
+++ src/components/add_blood_count/add-blood-count-component.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { Button, ButtonGroup } from "react-bootstrap";
+import { Button, ButtonGroup, Grid } from "react-bootstrap";
 
 export default class AddBloodCount extends React.Component {
   public render() {
     return (
       <div>
-        <h2>Add new blood count result:</h2>
-        <p>Todo - get bootstrap css working</p>
-        <ButtonGroup>
-          <Button bsClass="success">OK</Button>
-          <Button bsClass="warning">Cancel</Button>
-        </ButtonGroup>
+        <Grid>
+          <h2>Blood count result</h2>
+          <p>ToDo: Fields to include: date, counts, notes, star (allows the user to favourite this particular count)</p>
+          <ButtonGroup>
+            <Button bsStyle="success">OK</Button>
+            <Button bsStyle="warning">Cancel</Button>
+          </ButtonGroup>
+        </Grid>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-// import App from "./App";
-import AddBloodCount from "./components/add_blood_count/add-blood-count-component";
+import BloodCountDashboard from "./components/dashboard/dashboard-component";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<AddBloodCount />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(<BloodCountDashboard />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
