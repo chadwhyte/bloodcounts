3469aefe867c77f1157928d5d78dec03bca758ea
Chad Whyte <chad.whyte@gmail.com>
2018-11-07 21:03:21 -0700

    Generate test blood count data.

---
+++ src/components/blood_count/blood-count.interface.tsx
@@ -0,0 +1,9 @@
+export interface IBloodCount {
+  date: Date;
+  whiteBloodCount: number;
+  hemoglobin: number;
+  platelets: number;
+  absoluteNeutrophilCount: number;
+  dose?: number;
+  notes?: string;
+}
--- src/state/reducers/blood-count.reducer.tsx
+++ src/state/reducers/blood-count.reducer.tsx
@@ -1,6 +1,32 @@
+import { IBloodCount } from "../../components/blood_count/blood-count.interface";
 import { ADD_BLOOD_COUNT, GET_BLOOD_COUNTS } from "../actions/action-types";
 
 const initialState = [1, 2, 3, 4];
+const initialBloodCountState: IBloodCount[] = generateInitialBloodCounts();
+
+function generateInitialBloodCounts(): IBloodCount[] {
+  const bloodCounts: IBloodCount[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    bloodCounts.push(generateBloodCount());
+  }
+
+  return bloodCounts;
+}
+
+function generateBloodCount(): IBloodCount {
+  return {
+    absoluteNeutrophilCount: randomNumber(),
+    date: new Date("12/12/12"),
+    hemoglobin: randomNumber(),
+    platelets: randomNumber(),
+    whiteBloodCount: randomNumber()
+  };
+}
+
+function randomNumber(): number {
+  return Math.floor(Math.random() * 100) + 1;
+}
 
 export default function bloodCounts(state = initialState, action: any) {
   switch (action.type) {
