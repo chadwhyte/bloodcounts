98dbaddf4b77bd1632d8d42dee0de807d02b1409
Chad Whyte <chad.whyte@gmail.com>
2019-01-23 21:06:14 -0700

    Stubbing out custom legend

--- src/components/charts/chart-recharts.component.tsx
+++ src/components/charts/chart-recharts.component.tsx
@@ -1,14 +1,55 @@
 import * as moment from "moment";
 import * as React from "react";
+import { Checkbox } from "react-bootstrap";
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { IChartProps } from "./chart.interface";
 
-export default class ReChart extends React.Component<IChartProps, {}> {
+interface IChartState {
+  lineStatus: IChartLineStatus[];
+}
+
+interface IChartLineStatus {
+  dataKey: string;
+  visible: boolean;
+}
+
+export default class ReChart extends React.Component<IChartProps, IChartState> {
   constructor(props: IChartProps) {
     super(props);
   }
 
   public render() {
+    return (
+      <div>
+        {this.renderLegend()}
+        {this.renderChart()}
+      </div>
+    );
+  }
+
+  private renderLegend() {
+    const series = this.props.series;
+
+    return (
+      <div>
+        {series.map(line => {
+          return (
+            <Checkbox key={line.dataKey} color={line.stroke} name={line.dataKey} onClick={this.onLegendItemClicked}>
+              {line.name}
+            </Checkbox>
+          );
+        })}
+      </div>
+    );
+  }
+
+  // tslint:disable-next-line:no-empty
+  private onLegendItemClicked(event: any) {
+    // tslint:disable-next-line:no-console
+    console.log(event.target.name);
+  }
+
+  private renderChart() {
     return (
       <ResponsiveContainer height={500} width="100%">
         <LineChart data={this.props.data} margin={{ top: 30, right: 30, left: 0, bottom: 30 }}>
